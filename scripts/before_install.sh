7b0b4714db8bb1a5b2d02c11e0cbe4a6e440c351
Priyanka Patel <83409681+Priyanka-Cloud-Infinite@users.noreply.github.com>
2021-07-23 13:58:49 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-c=$(ls -l /var/www/html/ | wc -l)
+c=$(ls -l /var/www/testing2/ | wc -l)
 
 if [ $c -gt 0 ]
 then
-  cd /var/www/html
-  pm2 delete all
+  cd /var/www/testing2
+  
   rm -fr *
   rm -fr .git*
 else
